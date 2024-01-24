d4441764c1c4a72d3648143b7a5100a86f934b9a
Apocalypse-jk <kunz_jonas@outlook.de>
2024-01-24 14:50:01 +0100

    redirect to error page from invalid url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
@@ -14,6 +12,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [myLocation, setMyLocation] = useState<Location | null>(null);
+
+  useEffect(() => {
+    setMyLocation(window.location);
+    if(myLocation != null && myLocation.pathname != "/error" && myLocation.pathname != "/") {
+      myLocation.pathname = "/error";
+    }
+  });
+
   return (
     <html suppressHydrationWarning lang="en">
       {/*
@@ -24,9 +31,7 @@ export default function RootLayout({
 
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
         <Providers>
-          <Header />
           {children}
-          <Footer />
           <ScrollToTop />
         </Providers>
       </body>
@@ -35,3 +40,4 @@ export default function RootLayout({
 }
 
 import { Providers } from "./providers";
+import { useEffect, useState } from "react";
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import { Metadata } from "next";
 import About from "@/components/About";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Mainzelliste",
@@ -13,10 +15,12 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
+      <Header />
       <ScrollUp />
       <Hero />
       <About />
       <Contact />
+      <Footer />
     </>
   );
 }
