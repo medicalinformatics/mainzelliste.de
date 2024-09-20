a932dad0d590d13ad9fcf09845211723f3cdca2a
Moanes Ben Amor <m.benamor@dkfz-heidelberg.de>
2024-09-20 12:41:50 +0200

    ci: add path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['localhost','https://medicalinformatics.github.io/mainzelliste.de'],
+  },
+  basePath: "/mainzelliste.de",
   output: "export",
   reactStrictMode: true,
 };
